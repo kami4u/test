ea3f2270e01a46dc2f8692281d04eb22a86f5579
kami4u <kamran.gujjar@gmail.com>
2020-07-16 11:41:36 +0100

    Made app more robust responding with all errors

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -10,7 +10,10 @@ router.post(
   "/webhook",
   jsonParser,
   asyncRoute(async (req: Request, res: Response) => {
-    const result = [];
+    const result = {
+      data: [] as any,
+      errors: [] as any,
+    };
 
     if (!req.body.name || !req.body.callbackUrl) {
       res.status(404).end("Payload is not right need to have name and callbackUrl");
@@ -26,24 +29,38 @@ router.post(
     for (const ele of name) {
       try {
         const response = await axios.get(`http://127.0.0.1:3000/providers/${ele}`);
-        result.push({ name: ele, data: response.data });
+        result.data.push({ name: ele, data: response.data });
       } catch (error) {
-        if (error.code === "ECONNREFUSED") res.status(500).send("Provider server is not responding");
-        if (error.response.status === 404) {
-          res.status(error.response.status).send(`Provider ${ele} Does not exist`);
-          return;
-        }
-        if (error.response.status === 500) {
-          res.status(error.response.status).send(`Provider Api is Failing with message ${error.response.data}`);
-          return;
+        // Catch it if the server is not responding
+        if (error.code === "ECONNREFUSED") {
+          result.errors.push({
+            name: ele,
+            statusCode: res.status(500),
+            message: "Provider server is not responding",
+          });
+        } else if (error.response.status === 404) {
+          result.errors.push({
+            name: ele,
+            statusCode: error.response.status,
+            message: `Provider ${ele} Does not exist`,
+          });
+        } else if (error.response.status === 500) {
+          result.errors.push({
+            name: ele,
+            statusCode: error.response.status,
+            message: `Provider Api is Failing with message ${error.response.data}`,
+          });
         }
       }
     }
     try {
       await axios.post(callbackUrl, { result });
     } catch (error) {
-      res.status(404).send("Callback Url Does not Exist");
-      return;
+      result.errors.push({
+        name: "callbackUrl",
+        statusCode: 404,
+        message: "Callback Url Does not Exist",
+      });
     }
     res.send(result);
   }),
--- src/app.test.ts
+++ src/app.test.ts
@@ -17,24 +17,26 @@ describe("Provider", () => {
   it("Should give status 500 and error 'Provider Api is Failing with message #fail'", async () => {
     mock.onGet("http://127.0.0.1:3000/providers/gas").reply(500, "#fail");
     const res = await request.post("/api/webhook").send({ name: "gas", callbackUrl: "http://localhost:3100/api/data" });
-    expect(res.status).toBe(500);
-    expect(res.text).toBe("Provider Api is Failing with message #fail");
+    expect(res.status).toBe(200);
+    expect(res.body.errors[0].statusCode).toBe(500);
+    expect(res.body.errors[0].message).toBe("Provider Api is Failing with message #fail");
   });
 
   it("Should give status 404 and error 'Provider asd Does not exist'", async () => {
     mock.onGet("http://127.0.0.1:3000/providers/asd").reply(404, "#fail");
     const res = await request.post("/api/webhook").send({ name: "asd", callbackUrl: "http://localhost:3100/api/data" });
-    expect(res.status).toBe(404);
-    expect(res.text).toBe("Provider asd Does not exist");
+    expect(res.status).toBe(200);
+    expect(res.body.errors[0].statusCode).toBe(404);
+    expect(res.body.errors[0].message).toBe("Provider asd Does not exist");
   });
 
   it("Should give status 200 with the data", async () => {
     mock.onGet("http://127.0.0.1:3000/providers/gas").reply(200, gas);
     mock.onPost("http://localhost:3100/api/data").reply(200);
     const res = await request.post("/api/webhook").send({ name: "gas", callbackUrl: "http://localhost:3100/api/data" });
-
     expect(res.status).toBe(200);
-    expect(res.body[0].name).toEqual("gas");
+    expect(res.body.data[0].name).toBe("gas");
+    expect(res.body.data[0].data[0].data[0].amount).toBe(22.27);
   });
 
   it("Should give status 200 with the data containing all providers", async () => {
@@ -46,7 +48,9 @@ describe("Provider", () => {
       .send({ name: ["gas", "internal"], callbackUrl: "http://localhost:3100/api/data" });
 
     expect(res.status).toBe(200);
-    expect(res.body[0].name).toEqual("gas");
-    expect(res.body[1].name).toEqual("internal");
+    expect(res.body.data[0].name).toBe("gas");
+    expect(res.body.data[1].name).toEqual("internal");
+    expect(res.body.data[0].data[0].data[0].amount).toEqual(22.27);
+    expect(res.body.data[1].data[0].data[0].amount).toEqual(15.12);
   });
 });
